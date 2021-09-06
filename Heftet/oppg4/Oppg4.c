488ab93c1b532e1f3ac7acbe6580ca4b18aff83f
lokesvelland <lokesvelland@gmail.com>
2021-09-06 14:59:15 +0200

    eks7 oppg4

--- Heftet/oppg4/Oppg4.c
+++ Heftet/oppg4/Oppg4.c
@@ -6,10 +6,13 @@ int main() {
         mobilnummer = 95558305,
         alder       = 19;
 
+    char navn[] = "Loke Svelland";                          //char arrays med tekst
+    char adresse[] = "Ibsensgate 15";
+    char sted[] = "Gjøvik";
 
-    printf("Navn: \t\tLoke Svelland\n");                    //printer ut svar, har lagt inn tekst her
-    printf("Adresse: \tIbsensgate 15\n");                   //da jeg ikke får det til å fungere med
-    printf("Sted: \t\tGjøvik %i\n", postnummer);            //char/int
+    printf("Navn: \t\t%s\n", navn);                         //printer ut svar
+    printf("Adresse: \t%s\n", adresse);                     //la inn char array 6.9.21
+    printf("Sted: \t\t%s %i\n", sted, postnummer);            
     printf("Mobilnr: \t%i\n", mobilnummer);
     printf("Alder: \t\t%i\n", alder);
 
