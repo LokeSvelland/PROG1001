98e98af9b832c9e241dd52abff34bb51ca96e330
lokesvelland <lokesvelland@gmail.com>
2021-09-06 15:49:34 +0200

    mn

--- Heftet/oppg4/Oppg4.c
+++ Heftet/oppg4/Oppg4.c
@@ -6,9 +6,9 @@ int main() {
         mobilnummer = 95558305,
         alder       = 19;
 
-    char navn[] = "Loke Svelland";                          //char arrays med tekst
+    char navn[]    = "Loke Svelland";                        //char arrays med tekst
     char adresse[] = "Ibsensgate 15";
-    char sted[] = "Gjøvik";
+    char sted[]    = "Gjøvik";
 
     printf("Navn: \t\t%s\n", navn);                         //printer ut svar
     printf("Adresse: \t%s\n", adresse);                     //la inn char array 6.9.21
