4736d9a33be462578600da433419dc74931d50bc
Loke <lokesvelland@gmail.com>
2021-09-13 13:55:21 +0200

    Oblig1 ferdig!!!

--- Obliger/oblig1/oblig1.c
+++ Obliger/oblig1/oblig1.c
@@ -1,5 +1,11 @@
+/**
+ *   Obligatorisk oppgave nr.1
+ *
+ *   @file     Oblig1.c
+ *   @author   Loke Svelland
+ */
+
 #include <ctype.h>      //  Håndtering av enkelt-tegn(/bokstaver)
-#include <stdbool.h>    //  'bool' med 'true'/'false'  (istedet for '_Bool')
 #include <stdio.h>      //  printf, scanf, fil-håndtering
 #include <stdlib.h>	    //  Memory-håndtering, streng-konvertering, mer .....
 #include <string.h>	    //  Håndtering av char-arrayer/tekster/strenger
@@ -8,42 +14,45 @@ const int STRLEN = 15;
 
 int main() {
 
-    char bok1[STRLEN],
+    char bok1[STRLEN],  // Lager 2 arrays til bok navn
          bok2[STRLEN];
 
-    char sjanger[2];
+    char sjanger[2];    // array for sjanger, sidetall, sum og antall bøker
     int  sideTall[2];
     int  sum,
          antallBoker;
 
-    printf("Navn: ");           gets(bok1);
-    printf("Antall sider: ");   scanf("%i", &sideTall[0]);  getchar();   //  NB !
-    printf("sjanger (T - Fiksjon, T - Thriller, B - Biografi): "); scanf("%c", &sjanger[0]);
+                        //Her legges inn info til første bok
+    printf("Bok 1:\n");
+    printf("\tNavn: ");           gets(bok1);
+    printf("\tAntall sider: ");   scanf("%i", &sideTall[0]);  getchar();
+    printf("\tsjanger (T - Fiksjon, T - Thriller, B - Biografi): ");
+    scanf("%c", &sjanger[0]);
 
-    printf("\nInnleste verdier fra Bok nr 1\n");
-    printf("\tNavn:         %s\n", bok1);
-    printf("\tAntall sider: %i\n", sideTall[0]);
-    printf("\tSjanger:      %c\n", sjanger[0]);
+    getchar();          //getchar() for å hoppe over den /n som henger igjen
+                        //fra scanf
 
-    getchar();
+                        // Her legges inn info til bok nr 2
+    printf("\nBok 2:\n");
+    printf("\tNavn: ");         gets(bok2);
+    printf("\tAntall sider: ");   scanf("%i", &sideTall[1]);  getchar();
+    printf("\tsjanger (T - Fiksjon, T - Thriller, B - Biografi): ");
+    scanf("%c", &sjanger[1]);
 
-    printf("\nNavn: ");         gets(bok2);
-    printf("Antall sider: ");   scanf("%i", &sideTall[1]);  getchar();   //  NB !
-    printf("sjanger (T - Fiksjon, T - Thriller, B - Biografi): "); scanf("%c", &sjanger[1]);
+                        // printer ut navn og sjanger bok 1
+    printf("\nNavn:    \t%s\n", bok1);
+    printf("Sjanger:   \t%c\n", sjanger[0]);
 
-    printf("\nInnleste verdier fra Bok nr 2\n");
-    printf("\tNavn:         %s\n", bok2);
-    printf("\tAntall sider: %i\n", sideTall[1]);
-    printf("\tSjanger:      %c\n", sjanger[1]);
-
-    printf("\nNavn:    %s\n", bok1);
-    printf("Sjanger:   %c\n", sjanger[0]);
-
-    printf("\nNavn:    %s\n",   bok2);
-    printf("Sjanger:   %c\n",   sjanger[1]);
-
-   // antallBoker = bok1 + bok2;
+                        // printer ut navn og sjanger bok 2
+    printf("\nNavn:    \t%s\n",   bok2);
+    printf("Sjanger:   \t%c\n",   sjanger[1]);
+                        // plusser antall elementer i hver av arrayene så deler
+                        // på størrelsen til 1 array, STRLEN er 15 i begge
+                        // derfor kan jeg dele på 15 og finne hvor mange
+                        // bøker som er lagt til
+    antallBoker = (sizeof(bok1) + sizeof(bok2)) / 15;
     printf("\nBøker lest:        %i\n", antallBoker);
+                        // summerer sidetall
     sum = sideTall[0] + sideTall[1];
     printf("Sider lest totalt: %i\n", sum);
 
