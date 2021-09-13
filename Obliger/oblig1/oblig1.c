56118220bb3bcc7a9942166a0030884884b55c8e
lokesvelland <lokesvelland@gmail.com>
2021-09-13 13:01:30 +0200

    mangler antall bøker

--- Obliger/oblig1/oblig1.c
+++ Obliger/oblig1/oblig1.c
@@ -8,12 +8,45 @@ const int STRLEN = 15;
 
 int main() {
 
-    char bok1[STRLEN] = "Doppler", 
-         bok2[STRLEN] = "Min historie"; 
+    char bok1[STRLEN],
+         bok2[STRLEN];
 
-    char sjanger[] = {'B', 'F'};
-    int sideTall[] = {158, 559};
+    char sjanger[2];
+    int  sideTall[2];
+    int  sum,
+         antallBoker;
 
-    printf("Skriv inn navn på bok: ");   gets(bok1, bok2)
+    printf("Navn: ");           gets(bok1);
+    printf("Antall sider: ");   scanf("%i", &sideTall[0]);  getchar();   //  NB !
+    printf("sjanger (T - Fiksjon, T - Thriller, B - Biografi): "); scanf("%c", &sjanger[0]);
 
+    printf("\nInnleste verdier fra Bok nr 1\n");
+    printf("\tNavn:         %s\n", bok1);
+    printf("\tAntall sider: %i\n", sideTall[0]);
+    printf("\tSjanger:      %c\n", sjanger[0]);
+
+    getchar();
+
+    printf("\nNavn: ");         gets(bok2);
+    printf("Antall sider: ");   scanf("%i", &sideTall[1]);  getchar();   //  NB !
+    printf("sjanger (T - Fiksjon, T - Thriller, B - Biografi): "); scanf("%c", &sjanger[1]);
+
+    printf("\nInnleste verdier fra Bok nr 2\n");
+    printf("\tNavn:         %s\n", bok2);
+    printf("\tAntall sider: %i\n", sideTall[1]);
+    printf("\tSjanger:      %c\n", sjanger[1]);
+
+    printf("\nNavn:    %s\n", bok1);
+    printf("Sjanger:   %c\n", sjanger[0]);
+
+    printf("\nNavn:    %s\n",   bok2);
+    printf("Sjanger:   %c\n",   sjanger[1]);
+
+   // antallBoker = bok1 + bok2;
+    printf("\nBøker lest:        %i\n", antallBoker);
+    sum = sideTall[0] + sideTall[1];
+    printf("Sider lest totalt: %i\n", sum);
+
+
+    return 0;
 }
