e45ca88287426ac8fb566bcf7198cb536139ac2e
lokesvelland <lokesvelland@gmail.com>
2021-09-23 15:00:43 +0200

    hvordan gjøre oppg13og14

--- Heftet/oppg14/oppg14.c
+++ Heftet/oppg14/oppg14.c
@@ -3,25 +3,5 @@
 
 int main()
 {
-
-    int sum = 0, // variabler + arrays som brukes i programmet
-        i,
-        tall[10],
-        snitt = 0;
-
-    do
-    {
-        printf("Skriv inn kun positive tall: \n");
-        scanf("%d", &tall[10]);
-    } while (tall[10] > 0);
-    do
-    {
-        sum = 0;
-        (float) snitt = 0;
-        printf("negativt tall eller 0 skrevet inn, program stopper\n");
-        printf("alle innskrevne tall: %d\n", tall[10]);
-        printf("summen av alle tallene: %d\n", sum);
-        printf("gjennomsnitt av alle tallene: %.2f\n", snitt);
-        return 0;
-    } while (tall[10] == 0 || -tall[10]);
+    oppgaven sakl telle hvor mange ganger while løkken har loopet, så ta gjennomsnitt og sum
 }
