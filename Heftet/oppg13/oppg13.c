c88b9c7873e042ecfd430275fd40f75aae2f9d25
lokesvelland <lokesvelland@gmail.com>
2021-09-27 13:05:19 +0200

    oppg13og14

--- Heftet/oppg13/oppg13.c
+++ Heftet/oppg13/oppg13.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-                        // Oppgave ikke ferdi, da jeg ikke forstår 
-                        //oppgaven helt
 
-const int height = 4;   //konstant høyde 
 
-const int PI = 3.14;    //konstant PI
+const int hoyde = 4;     //konstant høyde
+const int PI = 3.14;     //konstant PI
+const int minVerdi = 5;  // min verdi som kan leses inn
+const int maxVerdi = 20; // maks verdi som kan leses inn
 
-int main() {
+int main()
+{
 
-bruke for løkke til å skrive ut volum og radie til sylinderet opp til verdien bruker har 
-skrevet inn, skal skrive ut alle verdiene
-
+    int volum, // tomme variabler
+        radie;
+                        // leser inn radie fra brukeren
+    printf("Skriv inn verdi mellom 5 og 20: \n");
+    scanf("%d", &radie);
+                        // går gjennom og regner ut hvert volum opptil bruker
+                        // sitt innleste tall
+    for (int i = 1; i <= radie; i++)
+    {
+                        // regner volum og ganger for hver gang loopen går
+        volum = 4 * PI * (i * radie ^ 2) * hoyde;
+                        // printer ut svar
+        printf("volum til sylinder: %i\n", volum);
+    }
 }
--- Heftet/oppg14/oppg14.c
+++ Heftet/oppg14/oppg14.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
-    oppgaven sakl telle hvor mange ganger while løkken har loopet, så ta gjennomsnitt og sum
+    // oppgaven skal telle hvor mange ganger while løkken har loopet, så ta gjennomsnitt og sum
+
+    int heltall,        // lager tomme variabler
+        snitt,
+        antall = 0,
+        sum;
+
+    while (heltall > 0) // leser inn positive heltall fra bruker
+    {
+        printf("Skriv inn et positivt heltall: \n");
+        scanf("%d", &heltall);
+        antall++;       // teller opp antall ganger det looper
+                        // definerer at det skal være positve tall
+        sum = heltall + antall;
+    } while (heltall <= 0) {
+        printf("antall ganger loopet: %i\n", antall);
+        printf("sum totalt av alle innleste heltall: %i\n", sum);
+        return 0;
+    }
 }
