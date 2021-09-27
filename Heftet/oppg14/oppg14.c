ec25b044b5f940a46fc75ec20e2fdc6fe1ebb0be
Loke <lokesvelland@gmail.com>
2021-09-27 15:33:52 +0200

    oppg14 ferdig

--- Heftet/oppg14/oppg14.c
+++ Heftet/oppg14/oppg14.c
@@ -2,23 +2,28 @@
 
 int main()
 {
-    // oppgaven skal telle hvor mange ganger while løkken har loopet, så ta gjennomsnitt og sum
 
-    int heltall,        // lager tomme variabler
-        snitt,
-        antall = 0,
-        sum;
+    int heltall,        // lager et tomt variabl
+        snitt = 0,      // setter verdiene som 0 her
+        antall = 0,     // oppdateres lenger nede i koden
+        sum = 0;
 
     while (heltall > 0) // leser inn positive heltall fra bruker
     {
         printf("Skriv inn et positivt heltall: \n");
         scanf("%d", &heltall);
         antall++;       // teller opp antall ganger det looper
+        sum += heltall;
                         // definerer at det skal være positve tall
-        sum = heltall + antall;
+                        // skver ut snitt, sum og antall ganger det har loopet
     } while (heltall <= 0) {
+                        // fjerner 1 fra antall slik at den ikke teller
+                        // med siste 0 / negative tall i loopen
+        antall = antall - 1;
         printf("antall ganger loopet: %i\n", antall);
         printf("sum totalt av alle innleste heltall: %i\n", sum);
+        snitt = sum / antall;
+        printf("Gjennomsnittet av innleste tall: %.2f", (float)snitt);
         return 0;
     }
 }
