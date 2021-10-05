447809a062d907da17174753339f8ca78e888545
Loke <lokesvelland@gmail.com>
2021-10-05 20:12:06 +0200

    fortsatt oblig 2

--- Obliger/Oblig2/oblig2.c
+++ Obliger/Oblig2/oblig2.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>	    //  Håndtering av char-arrayer/tekster/strenger
 
 const int MAXBANER = 18; ///< Maks antall baner.
 const int STRLEN = 100;  ///< Maks tekstlengde.
@@ -27,52 +29,56 @@ int main()
     bool baneOB[MAXBANER];                  // Har banene OB eller ei
     int antallBaner = 0,                    // Antall baner pr.nå.
         i,                                  // Hvilken bane vi er på
-        svarI;                              // tar imot int var fra bruker
-    char svar;                              // tar imot char svar fra bruker
+        svarI;                              // tar imot int svar fra bruker
+    char svar,                              // tar imot char svar fra bruker
+         valg;
+        
 
     //Bane 1
-    baneBeskrivelse[0][STRLEN] = "Bane med mange trær og kratt";
     baneLengde[0] = 62;
     banePar[0] = 3;
     baneOB[0] = true;
+    baneBeskrivelse[0][STRLEN] = "Bane med mange trær og kratt";
     antallBaner++;
 
     //Bane 2
-    baneBeskrivelse[1][STRLEN] = "Flatt terreng gjennom hele banen";
     baneLengde[1] = 94;
     banePar[1] = 3;
     baneOB[1] = false;
+    baneBeskrivelse[1][STRLEN] = "Flatt terreng gjennom hele banen";
     antallBaner++;
 
-    getchar();                              // Fjerner \n som ligger igjen
 
 
-    do
+    while (svar != 'Q')
     {
     printf("Meny:\nL - Legge til bane\nS - Se alle baner\nQ - Avslutt\n");
-    scanf("%c", &svar); svar = toupper(svar);// Leser innn brukers svar
-    
-        do
+    scanf("%c", &valg);
+    valg = toupper(valg); // Leser innn brukers svar
+        while (valg == 'L')
         {
-            if (antallBaner > MAXBANER)     // Skjekker om Max antall baner
-            {                               // er lagt til
+            if (antallBaner > MAXBANER) // Skjekker om Max antall baner
+            {                           // er lagt til
                 printf("Max antall baner lagt til\n");
-                return 'Q';
+                break;
             }
             else
-            {                               // legger til en bane om den er
-                antallBaner++;              // mindre enn MAXBANER
+            {                  // legger til en bane om den er
+                antallBaner++; // mindre enn MAXBANER
             }
-                                            // Leser inn lengde på banen
+            // Leser inn lengde på banen
             printf("Hvor lang er den %i.Banen: ", antallBaner);
             scanf(" %i", &svarI);
-            if(svarI <= 0) {
+            if (svarI <= 0)
+            {
                 printf("Ugyldig lengde, returnerer til meny.\n\n");
             }
-                                            // finner hvilken lomme i arrayet
-                                            // den skal ligge i
-            for (i = antallBaner - 1; i < MAXBANER; i++) {
-            baneLengde[i] = svarI;   }       // Oppdaterer lommen
+            // finner hvilken lomme i arrayet
+            // den skal ligge i
+            for (i = antallBaner; i < MAXBANER; i++)
+            {
+                baneLengde[i] = svarI;
+            } // Oppdaterer lommen
 
             svarI = 0;
             printf("Par på banen (2-8): ");
@@ -90,10 +96,12 @@ int main()
             scanf(" %d", &svar);
             toupper(svar);
             if(svar == 'J') {
-                baneOB[i] = true;
-            } else if(svar == 'N') {
-                baneOB[i] = false;
+                svar = atoi(1);
+            } else if (svar == 'N') {
+                svar = atoi(0);
             }
+            baneOB[i] = svar;
+
             getchar();
 
             svar = 0;
@@ -101,21 +109,30 @@ int main()
             scanf("%s", &svar);
             baneBeskrivelse[i][STRLEN] = svar;
 
-            getchar();
-        } while (svar = 'L');
 
-        do {
-            for(i = 0; i < antallBaner; i++) {
-                printf("Bane %i: \n", i);
-                printf("\tLengde: %i", baneLengde[i]);
-                printf("\tPar: %i", banePar[i]);
-                printf("\t%s", baneOB[i]);
-                printf("\tBeskrivelse: %s", baneBeskrivelse[i]);
+            break;
+        }
+
+        while (valg == 'S')
+        {
+            for (i = 0; i < antallBaner; i++)
+            {
+                printf("\nBane %i: \n", i + 1);
+                printf("\tLengde: %i\n", baneLengde[i]);
+                printf("\tPar: %i\n", banePar[i]);
+                if (baneOB[i] == 1)
+                {
+                    printf("\tMed OB\n");
+                }
+                else if (baneOB[i] == 0)
+                {
+                    printf("\tUten OB\n");
+                }
+                printf("\tBeskrivelse: %s\n", baneBeskrivelse[i][STRLEN]);
             }
-
-        } while (svar = 'S');
-
-    } while (svar != 'Q');
+            break;
+        }
+    }
     printf("\nProgram avsluttes.\n");
     return 0;
 }
